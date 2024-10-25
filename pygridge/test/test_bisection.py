016f27346e3b3af2f4495d86c0f30902b25b343e
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2024-10-24 22:19:50 -0400

    Fix relative import

--- pygridge/test/test_bisection.py
+++ pygridge/test/test_bisection.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pytest
 from numpy.testing import assert_almost_equal, assert_array_almost_equal
-from pygridge.src.bisection import BisectionSolver, BisectionError, ConvergenceError
+from ..src.bisection import BisectionSolver, BisectionError, ConvergenceError
 
 
 @pytest.fixture
